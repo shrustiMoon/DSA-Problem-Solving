6fbc9ab48f69eb88efcdb8a428f33bb8f5e7e716
Shrusti Moon <shrustimoon@gmail.com>
2026-01-26 01:15:17 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (20.19%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long reversenum = 0;
+        while(x!=0){
+            int ld = x % 10;
+            reversenum = (reversenum*10)+ld;
+            x = x / 10;
+        }
+        if(reversenum>INT_MAX || reversenum<INT_MIN){
+            return 0;
+        }
+        return (int)reversenum;
+    }
+};
