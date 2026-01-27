8cb0fde18672a9be690ab8818d96ba787aa2e2af
Shrusti Moon <shrustimoon@gmail.com>
2026-01-27 13:20:25 +0530

    Time: 386 ms (5.17%) | Memory: 21.7 MB (92.23%) - LeetSync

---
+++ 268-missing-number/missing-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        for(int i=1; i<=nums.size(); i++){
+            int flag = 0;
+            for(int j=0; j<nums.size(); j++){
+                if(nums[j]==i){
+                    flag = 1;
+                    break;
+                }
+            }
+            if(flag==0)
+            return i;
+        }
+        return 0;
+    }
+};
