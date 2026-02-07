960eee8f733406b3a2106b7c53ed04cb1b9d408f
Shrusti Moon <shrustimoon@gmail.com>
2026-02-07 12:24:36 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (16.17%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
-    bool check(int i, string &s){
-        if(i>=s.size()/2)
-           return true;
-        if(s[i] != s[s.size()-i-1]) 
-           return false;
-         return check(i+1,s);    
-    }
     bool isPalindrome(string s) {
-        string t ="";
+        string t;
         for(char c : s){
-            if(isalnum(c))
-            t += tolower(c);
+            if(isalnum(c)){
+                t.push_back(tolower(c));
+            }
         }
         return check(0,t);
+    }
+    bool check(int i, string &s){
+        if(i >= s.size()/2) return true;
+        if(s[i] != s[s.size()-i-1]) return false;
+        return check(i+1,s);
     }
 };
